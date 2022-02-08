1e7a312a3e223bf41b2702c524ed5303c67626ae
Alex173 <sashakechedzhy@gmail.com>
2022-02-08 12:59:33 +0200

    Commit 1. Base commit.

---
+++ Cattzo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cattzo
+//
+//  Created by Alex173 on 08.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
